3cb61e40d485566fe607f0989c3cc067ef9008f1
ChathuraML <cmlchathura@gmail.com>
2025-11-12 14:44:00 +0530

    action test

---
+++ auction/auction.py
@@ -0,0 +1,44 @@
+import os
+
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\
+
+'''
+
+print(logo)
+def find_highest_bidder(bidding_dic):
+    max = 0
+    winner = ""
+    for key in bidding_dic:
+        if bidding_dic[key] > max:
+            max = bidding_dic[key]
+            winner = key
+
+    print(f"The winner is {winner} with a bid of ${max}.")
+    
+auction = {}
+next = True
+while next:
+    name = input("What is your name?: ")
+    bid = int(input("\nWhat is your bid?: "))
+
+    auction[name] = bid
+
+    answer = input("Is there other users who want to bid(y/n)? ")
+    if answer == "y":
+        os.system('cls')
+    else:
+        next = False
+        os.system('cls')
+        find_highest_bidder(auction)
+
+
+#print(auction)
